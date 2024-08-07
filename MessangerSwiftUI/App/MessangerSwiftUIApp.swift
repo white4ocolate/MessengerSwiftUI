ceb10a0780341e2ce3ef5da99b8b1ab136c5e2ec
white4ocolate <morozovyuriy11@gmail.com>
2024-08-07 13:48:30 +0300

    init

---
+++ MessangerSwiftUI/App/MessangerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerSwiftUIApp.swift
+//  MessangerSwiftUI
+//
+//  Created by white4ocolate on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessangerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessangerSwiftUI/Core/Authenication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MessangerSwiftUI
+//
+//  Created by white4ocolate on 07.08.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}

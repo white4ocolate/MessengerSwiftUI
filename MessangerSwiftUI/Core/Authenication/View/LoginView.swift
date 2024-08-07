ff39f85a92bcdeffd4cf53528c2c8638cef9580a
white4ocolate <morozovyuriy11@gmail.com>
2024-08-07 18:10:47 +0300

    LoginUI

--- MessangerSwiftUI/Core/Authenication/View/LoginView.swift
+++ MessangerSwiftUI/Core/Authenication/View/LoginView.swift
@@ -8,8 +8,109 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                //login image
+                Image("messenger_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                //text fields
+                VStack(spacing: 12) {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(.rect(cornerRadius: 10))
+                        .padding(.horizontal, 24)
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .clipShape(.rect(cornerRadius: 10))
+                        .padding(.horizontal, 24)
+                }
+                
+                //forgot password
+                Button(action: {
+                    print("Forgot password")
+                }, label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                })
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                //login button
+                Button(action: {
+                    print("Pressed Login")
+                }, label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .clipShape(.rect(cornerRadius: 10))
+                })
+                .padding(.vertical)
+                
+                //sign in facebook
+                HStack {
+                    Rectangle()
+                        .frame(width: ((UIScreen.current?.bounds.width)! / 2) - 40 , height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: ((UIScreen.current?.bounds.width)! / 2) - 40 , height: 0.5)
+                }
+                .foregroundStyle(.gray)
+                
+                HStack {
+                    Image("facebook")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                // sign up
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign Up View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical)
+            }
+        }
     }
 }
 
---
+++ MessangerSwiftUI/Extensions/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Extensions.swift
+//  MessangerSwiftUI
+//
+//  Created by white4ocolate on 07.08.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension UIWindow {
+    static var current: UIWindow? {
+        for scene in UIApplication.shared.connectedScenes {
+            guard let windowScene = scene as? UIWindowScene else { continue }
+            for window in windowScene.windows {
+                if window.isKeyWindow { return window }
+            }
+        }
+        return nil
+    }
+}
+
+
+extension UIScreen {
+    static var current: UIScreen? {
+        UIWindow.current?.screen
+    }
+}

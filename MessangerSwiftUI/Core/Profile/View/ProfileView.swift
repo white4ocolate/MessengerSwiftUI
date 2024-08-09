cf17aaaeb6255c2294991f7d3819059ea6e619f0
white4ocolate <morozovyuriy11@gmail.com>
2024-08-09 18:11:18 +0300

    created ProfileView

---
+++ MessangerSwiftUI/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,68 @@
+//
+//  ProfileView.swift
+//  MessangerSwiftUI
+//
+//  Created by white4ocolate on 09.08.2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            
+            //header
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                        .foregroundStyle(Color(.systemGray4))
+                    ZStack {
+                        Circle()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(.white)
+                        Circle()
+                            .frame(width: 15, height: 15)
+                            .foregroundStyle(.green)
+                    }
+                }
+                Text("Bruce Wayne")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+            
+            //list
+            List {
+                Section {
+                    ForEach(0 ... 4, id: \.self) { option in
+                        HStack {
+                            Image(systemName: "bell.circle.fill")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(Color(.systemPurple))
+                            Text("Notifications")
+                                .font(.subheadline)
+                        }
+                        
+                    }
+                    
+                }
+                
+                Section {
+                    Button("Log Out") {
+                        print("Log out")
+                    }
+                    Button("Delete Account") {
+                        print("Delete Account")
+                    }
+                }
+                .foregroundStyle(.red)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
